ec8ca0bd6256dd0615ebb53c581ed2d98458f66f
Waseem Awashra <wawashra@gmail.com>
2023-02-18 13:18:19 +0200

    Creating Custom Permissions

--- store/admin.py
+++ store/admin.py
@@ -81,9 +81,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name',  'membership', 'orders']
     list_editable = ['membership']
     list_per_page = 10
-    ordering = ['first_name', 'last_name']
+    ordering = ['user__first_name', 'user__last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
-
+    
+    list_select_related = ['user']
     @admin.display(ordering='orders_count')
     def orders(self, customer):
         url = (
